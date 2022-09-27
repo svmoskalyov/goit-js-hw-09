c793eec273b5589b0043ae7278eb42700fd70026
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-27 16:36:39 +0300

    Завдання 1 - перемикач кольорів

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let intervalId = null;
+let isActive = false;
 
+buttonStart.addEventListener('click', onStartClick);
+buttonStop.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
