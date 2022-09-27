cdef0c543a736142d5b246b1a74c97a6eed74c4b
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-27 18:42:09 +0300

    Створює flatpickr

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,20 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+
+const buttonStart = document.querySelector('button[data-start]');
+const choseDate = document.querySelector('#datetime-picker');
+
+// buttonStart.disabled = true;
+
+buttonStart.addEventListener('click', onButtonStartClick);
+
+function onButtonStartClick() {
+  console.log('click start');
+}
+
+flatpickr(choseDate, {
+  enableTime: true,
+  dateFormat: 'Y-m-d H:i',
+  time_24hr: true,
+  //   minDate: new Date().fp_incr(1),
+});
