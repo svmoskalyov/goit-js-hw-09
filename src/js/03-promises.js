9ce3a443d607741199dcadd5ab809146a827a6b6
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-09-28 16:34:14 +0300

    task03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.currentTarget;
+  let inDelay = +delay.value;
+  const inStep = +step.value;
+  const inAmount = +amount.value;
+
+  for (let i = 1; i <= inAmount; i += 1) {
+    createPromise(i, inDelay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    inDelay += inStep;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    }
+    reject({ position, delay });
+  });
 }
